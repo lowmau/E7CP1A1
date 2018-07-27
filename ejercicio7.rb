e1f17a07c8860fcfc82802eaa04b3543ca3d99c5
lowmau <lowtjierivero@gmail.com>
2018-07-26 20:19:27 -0400

    Arreglos en el último ejercicio

--- ejercicio7.rb
+++ ejercicio7.rb
@@ -6,14 +6,14 @@ while(menu != 7) do
   puts "Opción 2: Eliminar"
   puts "Opción 3: Actualizar"
   puts "Opción 4: Stock total"
-  puts "Opción 5: Mayor cantidad de stock"
+  puts "Opción 5: Ítem con mayor cantidad de stock"
   puts "Opción 6: Preguntar si exite un item"
   puts "Opción 7: Salir"
   puts "Ingrese una opción"
   menu = gets.chomp.to_i
   case menu
 	when 1
-		puts "Ingrese el elemento a Agregar"
+		puts "Ingrese el ítem a Agregar"
 		a = gets.chomp
 		a = a.split(",")
 		p a[0]
@@ -21,38 +21,35 @@ while(menu != 7) do
 		inventario[a[0].to_sym] = a[1].to_i
 		puts inventario
 	when 2
-		puts "Ingrese el elemento a Eliminar"
+		puts inventario.keys
+		puts "Ingrese el ítem a Eliminar"
 		e = gets.chomp
+		inventario.delete(e.to_sym)
+		puts "Ítem eliminado"
+		puts inventario
 	when 3
-		puts 
+		puts inventario
+		puts "Ingrese el ítem que desea Actualizar"
+		act = gets.chomp
+		act = act.split(",")
+		inventario[act[0].to_sym] = act[1].to_i
+		puts inventario
 	when 4
-		puts 
+		stock = inventario.values
+		sum = 0
+		stock.each do |i|
+			sum += i 
+		end
+		p "El Stock Total es de: #{sum}"
 	when 5
-		puts 
+		alto = inventario.values.max
+		inv = inventario.invert
+		p "El ítem con mayor Stock es: #{inv[alto]}"
 	when 6
-		puts 
+		puts "Introduzca el ítem que desea consultar"
+		consult = gets.chomp.to_sym
+		p inventario.include?(consult)
 	else
 		puts 'Salir'
 	end
 end
-
-
-
-
-
-=begin
-menu = 0	
-while(menu != 4) do
-	if menu == 1
-		hash_new = {}
-		hash_new['pendrive'] = '100'
-		puts hash_new
-	end
-	if menu == 2
-		inventario.delete(:Notebooks)
-		puts inventario
-	end
-	if menu == 3
-	end
-end
-=end
